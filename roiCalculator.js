0c5a6669b22bce6c292606cbe6171d36b7710883
Alan Fudge <39415252+AlanFudge@users.noreply.github.com>
2022-02-22 15:47:40 -0600

    Add files via upload

---
+++ roiCalculator.js
@@ -0,0 +1,116 @@
+class roiCalculator {
+    constructor(elementSelector, options) {
+        const defaultOptions = {
+            revPerBedPerDay: 270,
+            insourcedBillRate: 0.0065,
+            billingRate: 0.03,
+            collectionRate: 0.98
+        };
+        this.options = Object.assign({}, defaultOptions, options);
+        
+        this.occupancyRate = 85
+        this.numOfHomes = 10;
+        this.bedsPerHome = 90;
+        this.insourcedCollectionsRate = .90;
+        
+        this.formElement = document.querySelector(elementSelector);
+
+        /*
+            inject HTML into document here
+
+            this.injectForm();
+
+            will need to have an empty div with enough attributes to use query selector
+        */
+
+        this.modal = document.querySelector(`${elementSelector} .roi-calc-modal-wrapper`);
+        this.modalInsourcedValues = document.querySelectorAll(`${elementSelector} .roi-calc-modal-results .insourced span`);
+        this.modalOutsourcedValues = document.querySelectorAll(`${elementSelector} .roi-calc-modal-results .outsourced span`);
+        this.modalDeltaValues = document.querySelectorAll(`${elementSelector} .roi-calc-modal-results .delta span`);
+        this.modalTotalROIValue = document.querySelector(`${elementSelector} .roi-calc-modal-results .roi span`);
+
+        this.closeModalButton = document.querySelector(`${elementSelector} .close-modal`);
+        this.closeModalButton.onclick = this.closeModal.bind(this);
+
+        this.formElement.onsubmit = this.handleSubmit.bind(this);
+    }
+
+    injectForm() {
+        this.formElement.innerHTML = (
+            //once design is finalized this should be changed to the needed HTML
+            `<form>
+                <h1>Hello World</h1>
+            </form>`
+        );
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        // Grab email in cms?
+
+        // inputs from user [number of homes, billing rate, collections rate, email] excludes submit input;
+        [this.numOfHomes, this.bedsPerHome, this.occupancyRate, this.insourcedCollectionsRate] = Object.values(e.target).slice(0, -1).map(node => Number.parseFloat(node.value));
+
+        this.occupancyRate = this.occupancyRate / 100;
+        this.insourcedCollectionsRate = this.insourcedCollectionsRate / 100;
+        const [insourcedResults, outsourcedResults, deltaResults, roi] = this.calculateResults();
+
+        this.modalInsourcedValues.forEach((element, i) => {
+            element.innerText = `$${Math.ceil(insourcedResults[i]).toLocaleString()}`;
+        });
+        this.modalOutsourcedValues.forEach((element, i) => {
+            element.innerText = `$${Math.ceil(outsourcedResults[i]).toLocaleString()}`;
+        });
+        this.modalDeltaValues.forEach((element, i) => {
+            element.innerText = `$${Math.ceil(deltaResults[i]).toLocaleString()}`;
+        });
+        this.modalTotalROIValue.innerText = `${roi} %`
+
+        // open modal
+        this.modal.style.display = 'flex';
+
+        return false;
+    }
+
+    closeModal(e) {
+        e.preventDefault();
+
+        return this.modal.style.display = 'none';
+    }
+
+    calculateResults() {
+        const submittedClaims = this.numOfHomes * this.bedsPerHome * this.occupancyRate * this.options.revPerBedPerDay;
+
+        const roundToThousands = (num) => {
+            return Math.round(num / 1000) * 1000;
+        }
+
+        //arrays hold [billing costs, gross collections, net collections] in that order
+        const insourced = [
+            submittedClaims,
+            submittedClaims * this.options.insourcedBillRate,
+            submittedClaims * this.insourcedCollectionsRate,
+            (submittedClaims * this.insourcedCollectionsRate) - (submittedClaims * this.options.insourcedBillRate)
+        ].map(roundToThousands);
+
+        const assembly = [
+            submittedClaims,
+            submittedClaims * this.options.billingRate,
+            submittedClaims * this.options.collectionRate,
+            (submittedClaims * this.options.collectionRate) - (submittedClaims * this.options.billingRate)
+        ].map(roundToThousands);
+
+        const delta = [
+            assembly[1] - insourced[1],
+            assembly[2] - insourced[2],
+            assembly[3] - insourced[3],
+        ];
+
+        const roi = Math.round((delta[1] / delta[0]) * 100);
+
+        return [insourced, assembly, delta, roi];
+    }
+}
+
+const myROI = new roiCalculator('#roi-calc');
