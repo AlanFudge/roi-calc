2af26dbe45dfc14518f2678a8237433d2ee4cf23
Alan Fudge <39415252+AlanFudge@users.noreply.github.com>
2022-04-29 12:26:09 -0500

    Update roiCalculator.js

--- roiCalculator.js
+++ roiCalculator.js
@@ -1,5 +1,9 @@
 "use strict";
 
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
 
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
@@ -40,14 +44,18 @@ var roiCalculator = /*#__PURE__*/function () {
           will need to have an empty div with enough attributes to use query selector
     */
 
+    this.emailInput = document.querySelector("".concat(elementSelector, " .roi-calc-input-wrapper input[type=email]"));
     this.modal = document.querySelector("".concat(elementSelector, " .roi-calc-modal-wrapper"));
     this.modalInsourcedValues = document.querySelectorAll("".concat(elementSelector, " .roi-calc-modal-results .insourced span"));
     this.modalOutsourcedValues = document.querySelectorAll("".concat(elementSelector, " .roi-calc-modal-results .outsourced span")); // this.modalDeltaValues = document.querySelectorAll(`${elementSelector} .roi-calc-modal-results .delta span`);
 
     this.modalDeltaNetCollectionsValue = document.querySelector("".concat(elementSelector, " .delta-bar span"));
     this.modalTotalROIValue = document.querySelector("".concat(elementSelector, " .roi-calc-modal-results .roi span"));
+    this.modalInsourcedBar = document.querySelector("".concat(elementSelector, " .insourced-bar"));
+    this.modalDeltaBar = document.querySelector("".concat(elementSelector, " .delta-bar"));
     this.closeModalButton = document.querySelector("".concat(elementSelector, " .close-modal"));
     this.closeModalButton.onclick = this.closeModal.bind(this);
+    this.sendEmailToHubspot = this.sendEmailToHubspot.bind(this);
     this.formElement.onsubmit = this.handleSubmit.bind(this);
   }
 
@@ -61,7 +69,8 @@ var roiCalculator = /*#__PURE__*/function () {
     key: "handleSubmit",
     value: function handleSubmit(e) {
       e.preventDefault(); // Grab email in cms?
-      // inputs from user [number of homes, billing rate, collections rate, email] excludes submit input;
+
+      this.sendEmailToHubspot(); // inputs from user [number of homes, billing rate, collections rate, email] excludes submit input;
 
       var _Object$values$slice$ = Object.values(e.target).slice(0, -1).map(function (node) {
         return Number.parseFloat(node.value);
@@ -94,6 +103,16 @@ var roiCalculator = /*#__PURE__*/function () {
       // swapped below out for above for requested bar graph
 
       this.modalDeltaNetCollectionsValue.innerText = "$".concat((Math.ceil(deltaResults[2]) / 1000).toLocaleString(), "K");
+      var percentageIncrease = Math.round((outsourcedResults[3] - insourcedResults[3]) / insourcedResults[3] * 100);
+      this.modalDeltaBar.style.width = '30%';
+      this.modalDeltaBar.style.width = "".concat(30 + percentageIncrease, "%");
+
+      if (percentageIncrease > 55) {
+        this.modalInsourcedBar.style.width = '5%';
+        this.modalDeltaBar.style.width = "".concat(5 + percentageIncrease, "%");
+      }
+
+      if (percentageIncrease > 85) this.modalDeltaBar.style.width = '85%';
       this.modalTotalROIValue.innerHTML = "<em>".concat(roi / 100, "X</em> ROI"); // open modal
 
       this.modal.style.display = 'flex';
@@ -121,6 +140,60 @@ var roiCalculator = /*#__PURE__*/function () {
       var roi = Math.round(delta[0] / delta[1] * 100);
       return [insourced, assembly, delta, roi];
     }
+  }, {
+    key: "sendEmailToHubspot",
+    value: function () {
+      var _sendEmailToHubspot = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+        var requestBody, response;
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                requestBody = {
+                  submittedAt: Date.now(),
+                  legalConsentOptions: {
+                    consent: {
+                      consentToProcess: true,
+                      text: "I agree to allow QHCR to store and process my personal data.",
+                      communications: [{
+                        value: true,
+                        subscriptionTypeId: 999,
+                        text: "I agree to receive marketing communications from QHCR."
+                      }]
+                    }
+                  },
+                  fields: [{
+                    objectTypeId: "0-1",
+                    name: "email",
+                    value: this.emailInput.value
+                  }]
+                };
+                _context.next = 3;
+                return fetch('https://api.hsforms.com/submissions/v3/integration/submit/20893444/6c83b0bd-f3cf-408d-b101-24bb5893f734', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify(requestBody)
+                });
+
+              case 3:
+                response = _context.sent;
+
+              case 4:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee, this);
+      }));
+
+      function sendEmailToHubspot() {
+        return _sendEmailToHubspot.apply(this, arguments);
+      }
+
+      return sendEmailToHubspot;
+    }()
   }]);
 
   return roiCalculator;
